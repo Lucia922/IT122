25b1396afd91387f938135da24f0ea3e42eba2e2
Jinhee Byoun <jinheebyoun@gmail.com>
2022-05-25 22:35:54 -0700

    Added API routes for getAll, getItem, delete movie and add movie

--- js_2/index.js
+++ js_2/index.js
@@ -10,7 +10,7 @@ import { Cinematheque } from "./Cinematheque.js";
 
 //import cinematheque from './data.js';
 //import querystring from 'querystring';
-import * as data from './data.js';
+//import * as data from './data.js';
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -19,6 +19,8 @@ app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json()); //Used to parse JSON bodies
 app.set('view engine', 'ejs'); // set the view engine to ejs
 
+import cors from 'cors';
+app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 
 // cinematheque server's 'home' Route to use MongoDb database
 app.get('/', (req, res, next) => {
@@ -30,12 +32,23 @@ app.get('/', (req, res, next) => {
     .catch(err => next(err))
 });
 
-// send static file as response
-// app.get('/', (req,res) => {
-//   res.type('text/html');
-//   res.render("home", {cinematheque: getAll()});
-//   //res.sendFile('./public/home.html');
-//  });
+//send static file as response
+app.get('/', (req,res) => {
+  res.type('text/html');
+  res.render("home", {cinematheque: getAll()});
+  //res.sendFile('./public/home.html');
+ });
+
+// api's getAll
+app.get('/api/cinematheque', (req,res) => {
+  try {
+    let cinematheque = getAll()
+    res.json(getAll());
+  } catch {
+    return res.status(500).send('Database Error Occurred');
+  }
+  //res.sendFile('./public/home.html');
+ });
 
 // cinematheque server's 'detail' Route to use MongoDb database
 app.get('/detail', (req,res,next) => {
@@ -47,53 +60,52 @@ app.get('/detail', (req,res,next) => {
       .catch(err => next(err));
 });
 
-// app.get('/detail', (req,res) => {
-//   res.type('text/html'); 
-//   console.log(req.query);
-//   let result = getItem(req.query.name);
-//   res.render("details", {name: req.query.name, result: result});
-//   });
-//   //res.sendFile('./public/home.html');
+//send static file as response
+app.get('/detail', (req,res) => {
+  res.type('text/html'); 
+  console.log(req.query);
+  let result = getItem(req.query.name);
+  res.render("details", {name: req.query.name, result: result});
+  });
+  //res.sendFile('./public/home.html');
 
-// // delete movie object 
-// app.get('/delete/:name', (req,res) => {
-//   Cinematheque.deleteOne ({name: req.params.name}, () => {
-//     res.send(req.params.name + " successfully deleted!");
-//   }); 
-// });
+//api's getItem
+app.get('/api/detail/:name', (req, res) => {
+    let name = req.params.name;
+    console.log(name);
+    Cinematheque.findOne({ name: name}, (err, result) => {
+      if (name) {
+        res.json(result);
+      } else {
+        return res.status(500).send('Database Error occurred');
+      }
+    });
+  });
 
-app.get('/delete/:name', (req,res) => {
-  cinematheque.remove ({name: req.query.name}, (err, result) => {
-     if (err) { return next (err)}
-     removeMovie ( req.query.name )
-     res.render ('delete', {name: req.query.name} )
-
-   });
+// Delete Object
+app.get('/delete/:name', (req, res, next) => {
+  Cinematheque.deleteOne ({'name': req.params.name}, (err, result) => {
+    if (err) return next(err);
+    // return # of items deleted
+    console.log(result)
+    res.json({"deleted": result});
+  });
 });
 
-// app.get('/delete/:name', (req, res, next) => {
-//   Cinematheque.deleteOne ({'name': req.params.id}, (err, result) => {
-//     if (err) return next(err);
-//     // return # of items deleted
-//     console.log(result)
-//     res.json({"deleted": result});
-//   });
-// });
+// api's Delete Object
+app.get('/api/delete/:name', (req, res) => {
+  Cinematheque.deleteOne ({'name':req.params.name}, (err, result) => {
+    if (err) {return res.status(500).send('Database Error occurred');}
+    else {
+      //console.log(result)
+    res.json({"deleted": result});
+    }
+  });
 
-// add movie object into mogodb using postman 1 - not work
-// app.post('/add', (req,res,next) => {
-//   // find & update existing movie, or add new
-//   console.log(req.body)
-//   if (!req.body.name) {
-//     // insert new document
-//     let movie = newMovie ({name:req.body.name, genre:req.body.genre, year:req.body.year, director:req.body.director, cast:req.body.cast});
-//     if (err) return next(err);
-//     console.log(newMovie)
-//     res.json({updated: 0, name: newMovie.name});
-//   }
-// });
+});
+  
 
-// add movie object into mogodb using postman 2
+// add movie object into mogodb using postman 
 app.post('/add', (req,res,next) => {
   const newMovie = {'name':req.body.name, 'genre':req.body.genre, 'year':req.body.year, 'director':req.body.director, 'cast':req.body.cast}
 
@@ -104,6 +116,35 @@ app.post('/add', (req,res,next) => {
   });
 
 });
+
+// api's add movie object
+app.post('/api/add', (req,res,next) => {
+  const newMovie = {'name':req.body.name, 'genre':req.body.genre, 'year':req.body.year, 'director':req.body.director, 'cast':req.body.cast}
+
+  Cinematheque.updateOne ({'name':req.body.name}, newMovie, {upsert:true}, (err, result) => {
+    if (err) return next(err);
+    //console.log(result);
+    //res.send( req.body.name + " movie added");
+    res.json({"Movie added": req.body.name + " movie added"});
+  });
+
+});
+
+// api's add movie object - test code
+// app.post('/api/add', (req,res,next) => {
+//   const newMovie = {'name':req.body.name, 'genre':req.body.genre, 'year':req.body.year, 'director':req.body.director, 'cast':req.body.cast}
+
+//   Cinematheque.updateOne ({'name':req.body.name}, newMovie, {upsert:true}, (err, result) => {
+//     if (err) {return res.status(500).send('Database Error occurred');}
+//     else {
+//       res.json({"Movie added": req.body.name + " movie added"});
+//     }
+    
+     
+//   });
+
+// });
+
 
 // app.post('/detail', (req,res) => {
 //   res.type('text/html');
@@ -137,6 +178,37 @@ app.use((req,res) => {
 app.listen(app.get('port'), () => {
   console.log('Express started');
  });
+
+///// Test Code 
+// // delete movie object 
+
+// app.get('/delete/:name', (req,res) => {
+//   Cinematheque.deleteOne ({name: req.params.name}, () => {
+//     res.send(req.params.name + " successfully deleted!");
+//   }); 
+// });
+
+// app.get('/delete/:name', (req,res) => {
+//   cinematheque.remove ({name: req.query.name}, (err, result) => {
+//      if (err) { return next (err)}
+//      removeMovie ( req.query.name )
+//      res.render ('delete', {name: req.query.name} )
+
+//    });
+// });
+
+// add movie object into mogodb using postman 1 - not work
+// app.post('/add', (req,res,next) => {
+//   // find & update existing movie, or add new
+//   console.log(req.body)
+//   if (!req.body.name) {
+//     // insert new document
+//     let movie = newMovie ({name:req.body.name, genre:req.body.genre, year:req.body.year, director:req.body.director, cast:req.body.cast});
+//     if (err) return next(err);
+//     console.log(newMovie)
+//     res.json({updated: 0, name: newMovie.name});
+//   }
+// });
 
 /*
 http.createServer(function(req,res){
